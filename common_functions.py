18f3d6c377946ead928d149ed43d610b802516a6
tarstarkvara <tarssander1@gmail.com>
2017-01-07 00:13:50 +0200

    Fixed data again... Added simple greedy if anyone needs.

--- common_functions.py
+++ common_functions.py
@@ -11,6 +11,7 @@ def readlessondata(filename):
         data = x.strip().split('\t')
         lessondata[data[-2]] += (data[:-2] + data[-1:])
         x = f.readline()
+    f.close()
     return lessondata
 ##struktuur tunniplaani väljundil (sonastike möll):
 #                                  päev1: [tund1:[[klass,tund,opetaja],[klass,tund,opetaja],...],
@@ -73,6 +74,47 @@ def evaluate_result(schedule, optimal_lessons):
             if max(lessons)>5:
                 score += 5*(max(lessons)-5)
     return score
+
+def greedy_simple(lessondata,max_days,max_hours, max_per_day):
+    ##lisa kontroll selle kohta, et kas tund on selles päevas juba olnud max_arv_kordi
+    lessonplan = {}
+    for i in range(max_days):
+        lessonplan['day' + str(i)] = {}
+        max_lessons = []
+        for j in range(max_hours):
+            lessonplan['day' + str(i)][j] = []
+            for k in lessondata:
+                if int(lessondata[k][3]) > 0:
+                    toadd = [lessondata[k][0],lessondata[k][1],lessondata[k][2]]
+                    addable = True
+                    if not [lessondata[k][0],lessondata[k][1]] in max_lessons:
+                        for l in lessonplan['day' + str(i)][j]:
+                            if lessondata[k][0] in l or (lessondata[k][1] and lessondata[k][2]) in l:
+                                addable = False
+                                break
+                        if addable:
+                            lessonplan['day' + str(i)][j].append(toadd)
+                            max_lessons.append([toadd[0],toadd[1]])
+                            lessondata[k][3] = str(int(lessondata[k][3]) - 1)
+    for i in range(max_days):
+        for j in range(max_hours):
+            for k in lessondata:
+                if int(lessondata[k][3]) > 0:
+                    toadd = [lessondata[k][0],lessondata[k][1],lessondata[k][2]]
+                    addable = True
+                    for l in lessonplan['day' + str(i)][j]:
+                        if lessondata[k][0] in l or (lessondata[k][1] and lessondata[k][2]) in l:
+                            addable = False
+                            break
+                    if addable:
+                        lessonplan['day' + str(i)][j].append(toadd)
+                        lessondata[k][3] = str(int(lessondata[k][3]) - 1)
+    test_result = []
+    for i in lessondata:
+        if int(lessondata[i][3]) > 0:
+            #print(lessondata[i])
+            test_result.append(lessondata[i])
+    return (lessonplan,test_result)
 
 print(readlessondata('Algo_Project_data.txt'))
 print(evaluate_result(test_dict,5))
